171a2d9a284822d7a58a4ebca8177b0150676c6e
DevXxmx <devxmxx@gmail.com>
2025-03-14 03:13:35 +0100

    Added project files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"/Travel-site/",
   plugins: [react(),
     tailwindcss(),  
   ],
